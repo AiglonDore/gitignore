0952f119859864ee757a5867b8d848fd268654cc
Thomas Roiseux <thomas.roiseux@outlook.com>
2023-08-15 19:28:02 +0200

    Added exception handling

--- __main__.py
+++ __main__.py
@@ -4,19 +4,16 @@ import asyncio
 import sys
 
 
-async def main() -> None:
-    """Main function."""
-    args = sys.argv[1:]
-    if not args or args[0] in ("-h", "--help"):
-        help.help()
-        return
-
-    if args[0] in ("-v", "--version"):
-        print("Python GigIgnore package version ", help.VERSION)
-        return
-
+async def make_git_ignore() -> None:
+    """Builds the file.
+    """
     print("Creating .gitignore file...")
-    files = await core.get_gitignore_list()
+
+    try:
+        files = await core.get_gitignore_list()
+    except Exception as e:
+        print(e, file=sys.stderr)
+        sys.exit(1)
 
     print("Writing .gitignore file...")
     with open(r".gitignore", "w") as f:
@@ -24,6 +21,19 @@ async def main() -> None:
             f.write(file + "\n")
     
     print("Done!")
+
+
+async def main() -> None:
+    """Main function."""
+    args = sys.argv[1:]
+    if not args or args[0] in ("-h", "--help"):
+        help.help()
+
+    elif args[0] in ("-v", "--version"):
+        print("Python GigIgnore package version ", help.VERSION)
+
+    else:
+        await make_git_ignore()
 
 
 if __name__ == "__main__":
