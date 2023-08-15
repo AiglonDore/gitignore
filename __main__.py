3d15aeb822734d1fae270ff884e2b7ae532e4ec0
Thomas Roiseux <thomas.roiseux@outlook.com>
2023-08-15 18:17:38 +0200

    Wrote main files

--- __main__.py
+++ __main__.py
@@ -1,1 +1,26 @@
 import core
+import core.help as help
+import asyncio
+import sys
+
+
+async def main() -> None:
+    """Main function."""
+    args = sys.argv[1:]
+    if not args or args[0] in ("-h", "--help"):
+        help.help()
+        return
+
+    if args[0] in ("-v", "--version"):
+        print("Python GigIgnore package version ", help.VERSION)
+        return
+
+    files = await core.get_gitignore_list()
+
+    with open(r".gitignore", "w") as f:
+        for file in files:
+            f.write(file + "\n")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,33 @@
+import asyncio
+import sys
 import aiohttp
+import async_lru
+
+URL = r"https://www.toptal.com/developers/gitignore/api/"
+
+
+@async_lru.alru_cache(maxsize=1024)
+async def get_gitignore(language: str) -> str:
+    """Get the `.gitignore` file for the selected language.
+
+    Args:
+        language (str): language.
+
+    Returns:
+        str: Git ignore file.
+    """
+    url = URL + language
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.text()
+
+
+async def get_gitignore_list() -> list[str]:
+    """Gets the files associated to the languages.
+
+    Returns:
+        list[str]: list of files.
+    """
+    return await asyncio.gather(
+        *[get_gitignore(lang) for lang in sys.argv[1:] if not lang.startswith("-")]
+    )
