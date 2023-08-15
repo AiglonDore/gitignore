0f81b703d45ddc692cfb1dedb34663c036224bee
Thomas Roiseux <thomas.roiseux@outlook.com>
2023-08-15 19:05:25 +0200

    Writing setup

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup, find_packages
-from core.help import VERSION
+
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+with open('README.md') as f:
+    readme = f.read()
 
 setup(
     name="gitignore",
-    version=VERSION,
+    version="1.0.0",
     description="A simple command line tool to generate .gitignore files",
     author="Aiglon Doré",
     author_email='aiglondore@outlook.com',
-    long_description=open('README.md').read(),
+    long_description=readme,
     long_description_content_type="text/markdown",
-    install_requires=open('requirements.txt').readlines(),
+    install_requires=requirements,
     packages=find_packages(),
 )
     
